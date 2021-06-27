12d34e910f2eeffcfd4df63574569a1c278831c5
kevinfAAA <kevin.foley21@hotmail.com>
2021-06-27 14:42:49 +0000

    Add sample questions to Geography Quiz Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,84 @@
-/*Google Charts*/
+/*Goegrapghy Quiz*/  
 
-google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawTitleSubtitle);
+//variables
+var quiz = [];
+quiz[0] = new Question("What is 1/4 of 100?", "25", "24", "23");
+quiz[1] = new Question("What color is blood?", "Red", "White", "Green");
+quiz[2] = new Question("What color is grass?", "Green", "White", "Red");
+quiz[3] = new Question("How many legs does a spider have?", "8", "6", "4");
+quiz[4] = new Question("Who is the king of the Netherlands?", "Willem-Alexander", "Willem-Alexzelf", "Willem-Alexniemand");
+quiz[5] = new Question("What is 2-2?", "0", "2", "4");
+quiz[6] = new Question("What was Vincent van Gogh?", "Artist", "Baker", "Jobless");
+var randomQuestion;
+var answers = [];
+var currentScore = 0;
 
-function drawTitleSubtitle() {
-      var data = google.visualization.arrayToDataTable([
-        ['City', '2010 Population', '2000 Population'],
-        ['New York City, NY', 8175000, 8008000],
-        ['Los Angeles, CA', 3792000, 3694000],
-        ['Chicago, IL', 2695000, 2896000],
-        ['Houston, TX', 2099000, 1953000],
-        ['Philadelphia, PA', 1526000, 1517000]
-      ]);
+document.addEventListener("DOMContentLoaded", function(event) { 
+  btnProvideQuestion();
+});
 
-      var materialOptions = {
-        chart: {
-          title: 'Population of Largest U.S. Cities',
-          subtitle: 'Based on most recent and previous census data'
-        },
-        hAxis: {
-          title: 'Total Population',
-          minValue: 0,
-        },
-        vAxis: {
-          title: 'City'
-        },
-        bars: 'horizontal'
-      };
-      var materialChart = new google.charts.Bar(document.getElementById('chart_div'));
-      materialChart.draw(data, materialOptions);
-    }
+function Question(question,rightAnswer,wrongAnswer1,wrongAnswer2) {
+    this.question = question;
+    this.rightAnswer = rightAnswer;
+    this.wrongAnswer1 = wrongAnswer1;
+    this.wrongAnswer2 = wrongAnswer2;
+};
+
+function shuffle(o) {
+	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
+
+function btnProvideQuestion() { 
+  
+	var randomNumber = Math.floor(Math.random()*quiz.length);
+	randomQuestion = quiz[randomNumber]; //getQuestion
+  answers = [randomQuestion.rightAnswer, randomQuestion.wrongAnswer1, randomQuestion.wrongAnswer2];
+  shuffle(answers);
+  
+  document.getElementById("question").innerHTML= randomQuestion.question;
+  document.getElementById("answerA").value= answers[0];
+  document.getElementById("answerA").innerHTML= answers[0];
+  document.getElementById("answerB").value= answers[1];
+  document.getElementById("answerB").innerHTML= answers[1];
+  document.getElementById("answerC").value= answers[2];
+  document.getElementById("answerC").innerHTML= answers[2];
+
+}
+
+function answerA_clicked() {
+  var answerA = document.getElementById("answerA").value;
+  	checkAnswer(answerA);
+}
+
+function answerB_clicked() {
+		var answerB = document.getElementById("answerB").value;
+  checkAnswer(answerB);
+}
+function answerC_clicked() {
+  var answerC = document.getElementById("answerC").value;
+  	
+		checkAnswer(answerC);
+}
+
+function adjustScore(isCorrect) {
+  debugger;
+  if (isCorrect) {
+    currentScore++;
+  } else {
+    if (currentScore > 0) {
+      currentScore--;
+  	}
+  }
+  document.getElementById("score").innerHTML = currentScore;
+}
+
+function checkAnswer(answer) {  
+  if (answer == randomQuestion.rightAnswer) {
+    adjustScore(true);
+    btnProvideQuestion();
+  } else { 
+    alert("Loser!");
+    adjustScore(false);
+  }	  
+}
