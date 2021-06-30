788a3e048ac4b1d5923c2769e2b1cf30ceebed80
kevinfAAA <kevin.foley21@hotmail.com>
2021-06-30 06:44:06 +0000

    Add timer to geography-easy html

--- assets/js/geography-easy.js
+++ assets/js/geography-easy.js
@@ -1,6 +1,6 @@
 /*Goegrapghy Quiz Easy*/  
 
-//variables
+// Quiz variables
 let quiz = [];
 quiz[0] = new Question("Which river flows through Paris?", "River Seine", "Put in wrong answer", "Put in wrong answer");
 quiz[1] = new Question("In which country is the UK’s highest mountain, Ben Nevis?", "Scotland", "Put in wrong answer", "Put in wrong answer");
@@ -81,7 +81,124 @@ function checkAnswer(answer) {
     adjustScore(true);
     btnProvideQuestion();
   } else { 
-    alert("Loser!");
+    alert("Wrong Answer, Try Again!!!");
     adjustScore(false);
   }	  
+}
+
+
+/*Timer*/
+
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const TIME_LIMIT = 3;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("timer").innerHTML = `
+<div class="quiz-timer">
+  <svg class="quiz-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="quiz-timer__circle">
+      <circle class="quiz-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="quiz-timer-path-remaining"
+        stroke-dasharray="283"
+        class="quiz-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="quiz-timer-label" class="quiz-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("quiz-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp(alert("Times Up, Please Try Again!!!"));
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("quiz-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("quiz-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("quiz-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("quiz-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("quiz-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
 }
